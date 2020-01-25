e13e341c4f57c526467c593833ba8e831ca5fe8d
Maxim Tkachenko <tkachenkonino1996@gmail.com>
2020-01-25 13:37:21 +0300

    генерирует поле со статистикой

--- js/stat.js
+++ js/stat.js
@@ -8,14 +8,8 @@ var GAPS = 10;
 var BAR_WIDTH = 40;
 var BAR_SPACE = 50;
 var FONT_GAP = 16;
-/* Ввожу функцию для создания рандомного процента
-ф-ия выводит рандомное значение от 0 до 1, округляет до 2 знаков и умножнает на 100%
-var getRandom = function () {
-  return Math.round(Math.random(), 2) *100%;
-};
-
-*/
-
+var MAX_BAR_HEIGHT = 150;
+//  определяем функцию генерации облака
 var renderCloud = function (ctx, x, y, color) {
   ctx.fillStyle = color;
   ctx.fillRect(x, y, CLOUD_WIDTH, CLOUD_HEIGHT);
@@ -30,31 +24,42 @@ var getMaxElement = function (arr) {
   }
   return maxElement;
 };
-
+  //  запуск генерации окна статистики
 window.renderStatistics = function (ctx, names, times) {
   //  определяем тень
   renderCloud(ctx, CLOUD_X + GAPS, CLOUD_Y + GAPS, 'rgba(0, 0, 0, 0.7)');
   //  определяем холст
   renderCloud(ctx, CLOUD_X, CLOUD_Y, '#fff');
-  //  определяем положение текста
+  //  определяем положение вступительного приветсвия
   ctx.fillStyle = '#000';
   ctx.font = '16px PT Mono';
-  ctx.fillText('Ура вы победили!', 110, 50);
-  ctx.fillText('Список результатов:', 110, 50 + FONT_GAP);
-
-  var maxTime = getMaxElement(times);
-
+  ctx.fillText('Ура вы победили!', CLOUD_X + GAPS * 3, CLOUD_Y + GAPS * 3);
+  ctx.fillText('Список результатов:', CLOUD_X + GAPS * 3, CLOUD_Y + GAPS * 3 + FONT_GAP);
+  //  генерируем график. с данными игры
   for (var i = 0; i < times.length; i++) {
     ctx.fillStyle = '#000';
-    ctx.fillText(names[i], 145 + (BAR_SPACE + BAR_WIDTH) * i, 250);
+    ctx.fillText(names[i], CLOUD_X + BAR_SPACE + (BAR_SPACE + BAR_WIDTH) * i, CLOUD_Y + GAPS * 6 + FONT_GAP * 2 + MAX_BAR_HEIGHT);
     if (names[i].indexOf('Вы') === 0) {
       ctx.fillStyle = 'rgba(255, 0, 0, 1)';
     } else {
-      ctx.fillStyle = 'hsl(240, 83%, 46%)';
-    /*  определяем положение текста
-     ctx.fillStyle = 'hsl(240, getRandom(), 46%)';
-    */
+      //  генерируем рандомное значение насыщенности
+      var getRandom = function () {
+        return Math.round(Math.random() * 100);
+      };
+      ctx.fillStyle = 'hsl(240,  ' + getRandom() + '%, 46%)';
     }
-    ctx.fillRect(145 + (BAR_SPACE + BAR_WIDTH) * i, 230, BAR_WIDTH, -(150 * times[i]) / maxTime);
+    var maxTime = getMaxElement(times);
+    ctx.fillRect(CLOUD_X + BAR_SPACE + (BAR_SPACE + BAR_WIDTH) * i, CLOUD_Y + GAPS * 6 + FONT_GAP + MAX_BAR_HEIGHT, BAR_WIDTH, -(MAX_BAR_HEIGHT * times[i]) / maxTime);
+    ctx.fillStyle = '#000';
+    //  генерируем функция округления времени
+    var roundTimes = [];
+    var getRoundTimes = function () {
+      for (var i = 0; i < times.length; i++) {
+        roundTimes[i] = Math.round(times[i]);
+      }
+      return roundTimes;
+    };
+    getRoundTimes();
+    ctx.fillText(roundTimes[i], CLOUD_X + BAR_SPACE + (BAR_SPACE + BAR_WIDTH) * i, CLOUD_Y + GAPS * 5 + FONT_GAP + MAX_BAR_HEIGHT - (MAX_BAR_HEIGHT * times[i]) / maxTime);
   }
 };
