e20df2f989ae5b051120ef1f2eab27c27aff04bc
Maxim Tkachenko <tkachenkonino1996@gmail.com>
2020-02-10 20:20:29 +0300

    добавляем модульность проекта

---
+++ js/dialog.js
@@ -0,0 +1,48 @@
+'use strict';
+(function () {
+  var ESC_KEY = 'Escape';
+  var ENTER_KEY = 'Enter';
+  var setup = document.querySelector('.setup');
+  var setupOpen = document.querySelector('.setup-open');
+  var setupClose = setup.querySelector('.setup-close');
+
+  var onPopupEscPress = function (evt) {
+    if (evt.key === ESC_KEY) {
+      closePopup();
+    }
+  };
+
+  var openPopup = function () {
+    setup.classList.remove('hidden');
+    document.addEventListener('keydown', onPopupEscPress);
+  };
+
+  var closePopup = function () {
+    setup.classList.add('hidden');
+    document.removeEventListener('keydown', onPopupEscPress);
+  };
+
+  setupOpen.addEventListener('click', function () {
+    openPopup();
+  });
+
+  setupOpen.addEventListener('keydown', function (evt) {
+    if (evt.key === ENTER_KEY) {
+      openPopup();
+    }
+  });
+
+  setupClose.addEventListener('click', function () {
+    closePopup();
+  });
+
+  setupClose.addEventListener('keydown', function (evt) {
+    if (evt.key === ENTER_KEY) {
+      closePopup();
+    }
+  });
+
+  window.dialog = {
+    onPopupEscPress: onPopupEscPress
+  };
+})();
