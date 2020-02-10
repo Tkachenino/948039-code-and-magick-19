782915a8503195d8a56b273cdd6a6139ebd12957
Maxim Tkachenko <tkachenkonino1996@gmail.com>
2020-02-10 23:01:26 +0300

    движение окна

--- js/dialog.js
+++ js/dialog.js
@@ -5,7 +5,6 @@
   var setup = document.querySelector('.setup');
   var setupOpen = document.querySelector('.setup-open');
   var setupClose = setup.querySelector('.setup-close');
-
   var onPopupEscPress = function (evt) {
     if (evt.key === ESC_KEY) {
       closePopup();
@@ -13,6 +12,8 @@
   };
 
   var openPopup = function () {
+    setup.setAttribute('top', 80);
+    setup.setAttribute('left', 637);
     setup.classList.remove('hidden');
     document.addEventListener('keydown', onPopupEscPress);
   };
@@ -41,6 +42,50 @@
       closePopup();
     }
   });
+  var dialogHandler = setup.querySelector('.upload');
+  dialogHandler.addEventListener('mousedown', function (evt) {
+    evt.preventDefault();
+    var startCoords = {
+      x: evt.clientX,
+      y: evt.clientY
+    };
+    var dragged = false;
+    var onMouseMove = function (moveEvt) {
+      moveEvt.preventDefault();
+      dragged = true;
+      var shift = {
+        x: startCoords.x - moveEvt.clientX,
+        y: startCoords.y - moveEvt.clientY
+      };
+
+      startCoords = {
+        x: moveEvt.clientX,
+        y: moveEvt.clientY
+      };
+
+      setup.style.top = (setup.offsetTop - shift.y) + 'px';
+      setup.style.left = (setup.offsetLeft - shift.x) + 'px';
+    };
+
+    var onMouseUp = function (upEvt) {
+      upEvt.preventDefault();
+
+      document.removeEventListener('mousemove', onMouseMove);
+      document.removeEventListener('mouseup', onMouseUp);
+
+      if (dragged) {
+        var onClickPreventDefault = function (clickEvt) {
+          clickEvt.preventDefault();
+          dialogHandler.removeEventListener('click', onClickPreventDefault);
+        };
+        dialogHandler.addEventListener('click', onClickPreventDefault);
+      }
+    };
+
+    document.addEventListener('mousemove', onMouseMove);
+    document.addEventListener('mouseup', onMouseUp);
+  });
+
 
   window.dialog = {
     onPopupEscPress: onPopupEscPress
