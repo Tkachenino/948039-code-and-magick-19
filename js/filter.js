1a9b31ef17c26fb0313a4d3938bddb95fc1fb736
Maxim Tkachenko <tkachenkonino1996@gmail.com>
2020-03-08 20:23:08 +0300

    filter

---
+++ js/filter.js
@@ -0,0 +1,37 @@
+'use strict';
+(function () {
+  var uniqWizards = [];
+  var form = document.querySelector('.setup-wizard-form');
+  var myWizard = document.querySelector('.setup-player');
+  var myWizardCoat = myWizard.querySelector('.wizard-coat');
+  var myWizardEyes = myWizard.querySelector('.wizard-eyes');
+  var myWizardFierBall = myWizard.querySelector('.setup-fireball-wrap');
+
+  var filterCoat = function (data) {
+    return myWizard.querySelector('[name="coat-color"]').value === data.colorCoat;
+  };
+
+  var filterEyes = function (data) {
+    return myWizard.querySelector('[name="eyes-color"]').value === data.colorEyes;
+  };
+
+  form.addEventListener('click', function () {
+    while (document.querySelector('.setup-similar-item')) {
+      document.querySelector('.setup-similar-item').remove();
+    }
+    console.log('check');
+    uniqWizards = window.dataFilter
+    .filter(filterCoat)
+    .filter(filterEyes)
+    .concat(window.dataFilter);
+    var fragment = document.createDocumentFragment();
+    uniqWizards = uniqWizards.slice(0, 4);
+    uniqWizards.forEach(function (item) {
+      var element = window.renderWizard(item);
+      fragment.appendChild(element);
+    });
+    window.similarListElement.appendChild(fragment);
+
+  });
+
+})();
